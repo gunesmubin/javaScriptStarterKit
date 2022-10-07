d0ccc7450db3dfb5141da314cd9bd34e865c4ca3
Mübin Güneş <mubin.gunes@pulur.com.tr>
2022-10-07 15:22:00 +0300

    JavaScript Started

--- src/app.js
+++ src/app.js
@@ -1,36 +1,20 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
+//JS TypeSafe değildir.
+let dolarBugun=9.30
+let dolarDun=9.20
+ dolarDun="Adana"
 {
-    let dolarDun = 9.10
+    let dolarDun=10
 }
-
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
-
+const euroDun=11.2
+// euroDun=11.9 sabit değişkenler sonradan  değiştirilemez.
 console.log(euroDun)
 
 //array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Memur Konut Kredisi"]
+for(let i=0;i<konutKredileri.length;i++){
+    console.log(konutKredileri[i])
 }
-console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
